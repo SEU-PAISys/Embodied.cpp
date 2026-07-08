c6a4e9933e4105605422273168162a781cae31da
feather1023 <1320363292@qq.com>
2026-07-08 17:58:31 +0800

    chore: update source attribution

--- eval/utils/service.py
+++ eval/utils/service.py
@@ -1,4 +1,5 @@
 # Copyright 2026 VinRobotics
+# Source: VinRobotics/vla.cpp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
